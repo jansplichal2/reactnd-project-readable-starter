42e7e699379eef390a9168b7ca4a133f3d3b5bca
jansplichal <yansplichal@gmail.com>
2017-08-19 20:39:58 +0200

    feat: Combining reducers and initial state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/app'
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
-import reducer from './reducers';
 import { Provider } from 'react-redux';
+import store from './store';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
 
 ReactDOM.render(<BrowserRouter>
                     <Provider store={store}>
