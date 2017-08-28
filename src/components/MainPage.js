f25713b61fa36ca24f37de37cb0f68c1020f5732
jansplichal <yansplichal@gmail.com>
2017-08-28 09:05:05 +0200

    feat: Layout improvements for the main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,13 +7,13 @@ const MainPage = () => {
     return (
         <div className="mt-4">
             <div className="row">
-                <div className="col-3">
+                <div className="col-12 col-md-3 col-lg-2 mb-3">
                     <CategoriesList/>
                 </div>
-                <div className="col-9">
+                <div className="col-12 col-md-9 col-lg-10">
                     <PostTable />
                 </div>
-                <div className="ml-auto col-9 my-4">
+                <div className="ml-auto col-12 col-md-9 col-lg-10 my-4">
                     <Link className="btn btn-lg btn-outline-primary" to="/posts/new">New Post</Link>
                 </div>
             </div>
